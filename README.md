d64e37792f221afcaae2aadd49f9c44b21b49d50
Craig <indigomx9@gmail.com>
2022-05-20 19:33:07 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Redux-Toolkit
+
+Intial setup of the exercice.  Created with Vite, React, Router 6, and Redux-Toolkit.
+
+
---
+++ src/app/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae commodi dicta minus aliquid ab accusantium quidem, quos deserunt facilis omnis doloribus nisi porro culpa nostrum quasi eum fuga quo dolorem.</p>
+        </React.Fragment>
+    );
+};
+
+
+
